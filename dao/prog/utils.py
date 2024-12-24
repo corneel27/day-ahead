f56e8fe10b4c1364ade488198ad2a5a6fabd52d8
Cees van Beek <16610333+corneel27@users.noreply.github.com>
2024-12-24 13:34:58 +0100

    Update utils.py

--- dao/prog/utils.py
+++ dao/prog/utils.py
@@ -210,7 +210,7 @@ def get_tibber_data():
     ]
     tibber_df = pd.DataFrame(columns=["time", "code", "value"])
     for node in production_nodes:
-        timestamp = int(get_datetime_from_str(node["from"]).timestamp()
+        timestamp = int(get_datetime_from_str(node["from"]).timestamp())
         if timestamp < today_ts:
             time_stamp = str(timestamp)
             if not (node["production"] is None):
