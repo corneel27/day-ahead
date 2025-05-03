7838fdcd6543dd3457462e262dc85e48c17f5ead
corneel27 <cees.van.beek@xs4all.nl>
2025-05-03 22:29:37 +0200

    Versie 2025.5.0 rc1 - Introduction of a check on "last invoice", warning when more than one year ago. - Fix Dockerfile for build error Pillow library

--- dao/docker_build.sh
+++
@@ -1,6 +0,0 @@
-docker buildx build \
-   --build-arg BUILD_ARCH=amd64 \
-   --build-arg BUILD_FROM=ghcr.io/hassio-addons/debian-base/amd64:stable \
-   --build-arg BUILD_VERSION=2025.3.1 \
-   -t dao:latest \
-   https://github.com/corneel27/day-ahead.git#main:dao
