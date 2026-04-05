7a5a5025e7b5a1f4c7c1a3fa8023172fc1240402
corneel27 <cees.van.beek@xs4all.nl>
2026-04-05 11:00:54 +0200

    restored bashio::log for echo

--- dao/run/run.sh
+++ dao/run/run.sh
@@ -4,7 +4,7 @@ set -e
 
 dir="/config/dao_data"
 if [ ! -d "$dir" ]; then
-  echo "=> directory dao_data made, files copied"
+  bashio::log.info "=> directory dao_data made, files copied"
   cp -r /tmp/daodata /config/dao_data
   file=/config/dao_data/options.json
   if [ ! -L "$file" ]; then
@@ -15,16 +15,16 @@ if [ ! -d "$dir" ]; then
     cp /config/dao_data/secrets_vb.json $file
   fi
 else
-  echo"=> directory dao_data exist"
+  bashio::log.info"=> directory dao_data exist"
 fi
 
 cd /root/dao/prog
 file=../data
 if [ -L "$file" ]
 then
-  echo "=> /root/dao/data exist"
+  bashio::log.info "=> /root/dao/data exist"
 else
-  echo "=> /root/dao/data doesn't exist, made"
+  bashio::log.info "=> /root/dao/data doesn't exist, made"
   ln -s /config/dao_data $file
 fi
 
@@ -32,22 +32,22 @@ cd /root/dao/webserver/
 file=app/static/data
 if [ -L "$file" ]
 then
-  echo "=> /root/dao/webserver/app/static/data exist"
+  bashio::log.info "=> /root/dao/webserver/app/static/data exist"
 else
-  echo "=> /root/dao/webserver/app/static/data doesn't exist, made"
+  bashio::log.info "=> /root/dao/webserver/app/static/data doesn't exist, made"
   ln -s /config/dao_data $file
 fi
 
 export PYTHONPATH="/root:/root/dao:/root/dao/lib:/root/dao/prog"
 cd /root/dao/prog
-python3 check_db.py || { echo "check_db.py failed, exiting"; sleep 5; exit 1; }
+python3 check_db.py || { bashio::log.info "check_db.py failed, exiting"; sleep 5; exit 1; }
 
 if [bashio::config.true 'use_self_compiled_miplib'; then
   if [ -d /config/miplib/lib ]; then
-    echo "Copying saved miplib-binaries"
+    bashio::log.info "Copying saved miplib-binaries"
     cp -a /config/miplib/lib/*.so /root/dao/prog/miplib/lib
   else
-    echo "Building new miplib-binaries"
+    bashio::log.info "Building new miplib-binaries"
     bash ./build_mip.sh
   fi
   export PMIP_CBC_LIBRARY="/root/dao/prog/miplib/lib/libCbc.so"
