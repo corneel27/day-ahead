e3b111e777211b1f38bea604f1a2993e1e427108
storeman <81527+storeman@users.noreply.github.com>
2026-08-10 23:23:57 +0200

    Fix: UI flickering in dark mode (#764)
    Co-authored-by: storeman <martin@wi-ke.nl>

--- dao/webserver/assets/main.js
+++ dao/webserver/assets/main.js
@@ -128,60 +128,6 @@ document.querySelectorAll('.tom-select').forEach((el) => {
 // Eigen styling als laatste
 import './main.scss'
 
-/*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2025 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- */
-
-(() => {
-    'use strict'
-
-    const getStoredTheme = () => localStorage.getItem('theme')
-    const setStoredTheme = theme => localStorage.setItem('theme', theme)
-
-    const getPreferredTheme = () => {
-        const storedTheme = getStoredTheme()
-        if (storedTheme) {
-            return storedTheme
-        }
-
-        return 'auto'
-    }
-
-    const setTheme = theme => {
-        document.documentElement.setAttribute('data-bs-stored-theme', theme)
-        if (theme === 'auto') {
-            document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
-        } else {
-            document.documentElement.setAttribute('data-bs-theme', theme)
-        }
-    }
-
-    setTheme(getPreferredTheme())
-
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-        const storedTheme = getStoredTheme()
-        if (storedTheme !== 'light' && storedTheme !== 'dark') {
-            setTheme(getPreferredTheme())
-        }
-    })
-
-    window.addEventListener('DOMContentLoaded', () => {
-        document.querySelectorAll('[data-bs-theme-value]')
-            .forEach(toggle => {
-                toggle.addEventListener('click', () => {
-                    const theme = toggle.getAttribute('data-bs-theme-value')
-                    console.log(theme)
-                    setStoredTheme(theme)
-                    setTheme(theme)
-                })
-            })
-    })
-
-})()
-
-
 window.toDatetimeLocalValue = (date, withTime = true) => {
     const pad = n => String(n).padStart(2, '0');
 
